3079919a3297bea4013330acb49599a0749f664a
Nicholas <nicholas@nicholasjhana.com>
2020-05-28 20:50:03 +0200

    added create table sql option

--- airflows/plugins/operators/load_facts.py
+++ airflows/plugins/operators/load_facts.py
@@ -18,6 +18,7 @@ class LoadFactOperator(BaseOperator):
                  # Example:
                  # conn_id = your-connection-name
                  table_id='',
+                 create_table_sql=None,
                  redshift_conn_id='',
                  sql_select='',
                  *args, **kwargs):
@@ -26,6 +27,7 @@ class LoadFactOperator(BaseOperator):
         # Example:
         # self.conn_id = conn_id
         self.table = table_id
+        self.create_table_sql=create_table_sql
         self.redshift_conn_id = redshift_conn_id
         self.sql_select = sql_select
 
@@ -33,6 +35,11 @@ class LoadFactOperator(BaseOperator):
         self.log.info(f'Loading table {self.table}')
 
         redshift_hook = PostgresHook(self.redshift_conn_id)
+
+        if self.create_table_sql is not None:
+            redshift_hook.run(f"DROP TABLE IF EXISTS {self.table}")
+            redshift_hook.run(f"{self.create_table_sql}")
+
         sql_stmt = LoadFactOperator.sql.format(
             self.table,
             self.sql_select
